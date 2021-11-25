0606cf4b9f44b8800fd8b8e3b12190c69f4a7a27
smidovic <msmidovic@gmail.com>
2021-11-25 10:34:29 +0100

    mazanie interpunkcii

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -5,8 +5,48 @@ namespace Open_Lab_05._02
     public class StringTools
     {
         public string NoYelling(string sentence)
-        {
-            throw new NotImplementedException();
+        {            
+            int pocetInterpunkcii = 0;
+            int miestoInterpunkcie = 0;
+            if (sentence.Contains("!"))
+            {
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i] == '!')
+                    {
+                        pocetInterpunkcii++;
+                    }
+                }
+                if (pocetInterpunkcii > 1) //na mazanie
+                {
+                    int lenNaMazanie = pocetInterpunkcii - 1;
+                    miestoInterpunkcie = sentence.IndexOf("!") + 1;
+                    sentence = sentence.Remove(miestoInterpunkcie, lenNaMazanie);
+                }
+            }
+            else if (sentence.Contains("?"))
+            {
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i] == '?')
+                    {
+                        pocetInterpunkcii++;
+                        miestoInterpunkcie = i;
+                    }
+                }
+                if (pocetInterpunkcii > 1) //na mazanie
+                {
+                    int lenNaMazanie = pocetInterpunkcii - 1;
+                    miestoInterpunkcie = sentence.IndexOf("?") + 1;
+                    sentence = sentence.Remove(miestoInterpunkcie, lenNaMazanie);
+                }
+            }
+
+            return sentence;
+
+
+
+
         }
     }
 }
